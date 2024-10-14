6f67369b211273737227e01c8ac08667894f546e
tobiasgrantner <tobias.grantner@gmail.com>
2024-10-14 11:42:46 +0200

    Move python files to src folder

---
+++ src/benchmark/process.py
@@ -0,0 +1,18 @@
+import subprocess
+from subprocess import CompletedProcess, DEVNULL
+from typing import Callable
+
+
+def run(
+    command: str, shell=True, check=True, stdout=DEVNULL, stderr=DEVNULL, *kwargs
+) -> CompletedProcess[bytes]:
+    return subprocess.run(
+        command, shell=shell, check=check, stdout=stdout, stderr=stderr, *kwargs
+    )
+
+
+def runnable(command: str) -> Callable[[], CompletedProcess[bytes]]:
+    def callable(*_) -> CompletedProcess[bytes]:
+        return run(command, shell=True, check=True, stdout=DEVNULL, stderr=DEVNULL)
+
+    return callable
