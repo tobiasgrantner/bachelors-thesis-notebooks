f9123a0f153bfcea14df5f64287bcf45a1a97674
tobiasgrantner <tobias.grantner@gmail.com>
2024-11-06 21:13:47 +0100

    Updated file reading functions

--- src/benchmark/file.py
+++ src/benchmark/file.py
@@ -1,18 +1,27 @@
 from os import PathLike
-from typing import Iterable
+from pathlib import Path
+from typing import Iterable, Tuple
 import pandas as pd
 
 
-def read_csv(filenames: Iterable[str | PathLike]):
+def read_results(
+    action: str, improvement: str, state: str, directory: PathLike = Path()
+) -> pd.DataFrame:
+    df = pd.read_csv(directory / f"{action}_{improvement}_{state}.csv")
+    df.columns = df.columns.str.capitalize()
+    df["Duration"] = df["Duration"] / 1e9
+    df["Action"] = action.replace("-", " ")
+    df["Improvement"] = improvement.replace("-", " ").capitalize()
+    df["State"] = state.capitalize()
+    return df
+
+
+def concat_results(
+    action: str, states: Iterable[Tuple[str, str]], directory: PathLike = Path()
+) -> pd.DataFrame:
     dfs = []
 
-    for filename in filenames:
-        df = pd.read_csv(filename)
-        df["type"] = str(filename).split("/")[-1].split(".")[0]
-        dfs.append(df)
+    for improvement, state in states:
+        dfs.append(read_results(action, improvement, state, directory))
 
     return pd.concat(dfs)
-
-
-def agg_csv(filenames: Iterable[str | PathLike], agg="mean"):
-    return read_csv(filenames).groupby("type").agg(agg)
