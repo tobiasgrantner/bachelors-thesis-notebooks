0a6f539b64a0202fcecded942d26abb75beec299
tobiasgrantner <tobias.grantner@gmail.com>
2024-11-09 17:19:47 +0100

    Code duplication analysis

--- src/benchmark/process.py
+++ src/benchmark/process.py
@@ -4,15 +4,15 @@ from typing import Callable
 
 
 def run(
-    command: str, shell=True, check=True, stdout=DEVNULL, stderr=DEVNULL, *kwargs
+    command: str, shell=True, check=True, stdout=DEVNULL, stderr=None, **kwargs
 ) -> CompletedProcess[bytes]:
     return subprocess.run(
-        command, shell=shell, check=check, stdout=stdout, stderr=stderr, *kwargs
+        command, shell=shell, check=check, stdout=stdout, stderr=stderr, **kwargs
     )
 
 
 def runnable(command: str) -> Callable[[], CompletedProcess[bytes]]:
     def callable(*_) -> CompletedProcess[bytes]:
-        return run(command, shell=True, check=True, stdout=DEVNULL, stderr=DEVNULL)
+        return run(command, shell=True, check=True, stdout=DEVNULL, stderr=None)
 
     return callable
